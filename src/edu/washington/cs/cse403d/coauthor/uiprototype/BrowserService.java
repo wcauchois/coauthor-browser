67e421de35f092148513768c82f8ccbd5d6175ae
William Cauchois <wcauchois@gmail.com>
2010-04-20 23:18:46 -0700

    BrowserService methods should be static (oops)

--- src/edu/washington/cs/cse403d/coauthor/uiprototype/BrowserService.java
+++ src/edu/washington/cs/cse403d/coauthor/uiprototype/BrowserService.java
@@ -1,13 +1,13 @@
 package edu.washington.cs.cse403d.coauthor.uiprototype;
 
 public class BrowserService {
-	private Browser browse;
-	public Browser getBrowser() {
+	private static Browser browse;
+	public static Browser getBrowser() {
 		if(browse == null)
 			throw new RuntimeException("there is no available browser");
 		return browse;
 	}
-	public void provideBrowser(Browser browse) {
-		this.browse = browse;
+	public static void provideBrowser(Browser theBrowser) {
+		browse = theBrowser;
 	}
 }
