f6a314b2203940a31523e8b41b4ca412ca6dca39
William Cauchois <wcauchois@gmail.com>
2010-04-26 17:30:42 -0700

    added CoauthorDataServiceInterface to Services & initialization

--- src/edu/washington/cs/cse403d/coauthor/uiprototype/Services.java
+++ src/edu/washington/cs/cse403d/coauthor/uiprototype/Services.java
@@ -1,9 +1,12 @@
 package edu.washington.cs.cse403d.coauthor.uiprototype;
 
+import edu.washington.cs.cse403d.coauthor.dataservice.CoauthorDataServiceInterface;
+
 // Used to keep track of a global instance of Browser.
 public class Services {
 	private static Browser theBrowser;
 	private static ResourceManager theResourceManager;
+	private static CoauthorDataServiceInterface theCoauthorDataServiceInterface;
 	
 	public static ResourceManager getResourceManager() {
 		if(theResourceManager == null)
@@ -15,7 +18,14 @@ public class Services {
 			throw new RuntimeException("There is no available browser");
 		return theBrowser;
 	}
+	public static CoauthorDataServiceInterface getCoauthorDataServiceInterface() {
+		return theCoauthorDataServiceInterface;
+	}
 	
+	public static void provideCoauthorDataServiceInterface(
+			CoauthorDataServiceInterface coauthorDataServiceInterface) {
+		theCoauthorDataServiceInterface = coauthorDataServiceInterface;
+	}
 	public static void provideResourceManager(ResourceManager resourceManager) {
 		theResourceManager = resourceManager;
 	}
