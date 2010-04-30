cdd34df0794352717a198cbffe8992640c23f273
Kevin Bang <Kevin Bang@localhost>
2010-04-30 11:51:05 -0700

    couple changes

--- src/edu/washington/cs/cse403d/coauthor/uiprototype/SearchResultsPage.java
+++ src/edu/washington/cs/cse403d/coauthor/uiprototype/SearchResultsPage.java
@@ -1,12 +1,23 @@
 package edu.washington.cs.cse403d.coauthor.uiprototype;
 
+import java.util.List;
+
 public class SearchResultsPage extends BrowserPage {
-	private String query;
+	//private String query;
+	private List<String> queries;
+	/*
 	public SearchResultsPage(String query) {
 		this.query = query;
+	}*/
+	
+	public SearchResultsPage(List<String> queries) {
+		this.queries = queries;
+		System.out.println("Moved to search pane");
+		this.setVisible(true);
 	}
+	
 	public String getTitle() {
-		return "Results for \"" + query + "\"";
+		return "Results for \"" + queries.get(0) + "\"";
 	}
 	@SuppressWarnings("unchecked")
 	public Class[] getCrumbs() {
