e07720f5115352a68a9fae8b43fb25f92ada1a28
Kevin Bang <yusbang@gmail.com>
2010-05-03 09:46:14 -0700

    Prints the article list now

--- src/edu/washington/cs/cse403d/coauthor/uiprototype/AuthorSearchResults.java
+++ src/edu/washington/cs/cse403d/coauthor/uiprototype/AuthorSearchResults.java
@@ -19,98 +19,21 @@ import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import edu.washington.cs.cse403d.coauthor.shared.model.Publication;
+
 public class AuthorSearchResults extends BrowserPage {
 	private List<String> queries;
 	private edu.washington.cs.cse403d.coauthor.shared.CoauthorDataServiceInterface CDSI = 
 		Services.getCoauthorDataServiceInterface();
-	/*
-	 * This will produce the search result screen with single-entry
-	 * 	author search.
-	 */
-	private class SingleAuthorSearchResult extends JPanel implements ListSelectionListener{
-		private String theAuthor;
-		private List<String> coauthors;
-		private DefaultListModel listModel;
-		private JList coauthorList;
-		
-		public SingleAuthorSearchResult(String author) {
-			this.theAuthor = author;
-			initialize();
-			printTest();
-		}
-		
-		private void initialize() {
-			setVisible(true);			
-			JLabel author = new JLabel(theAuthor);
-			author.addMouseListener(new MouseAdapter() {
-				public void mouseClicked(MouseEvent evt) {
-					Services.getBrowser().go(new AuthorSearchResults(createList(theAuthor)));
-				}
-			});
-			add(author);
-			
-			listModel = new DefaultListModel();
-			coauthorList = new JList(listModel);
-			try {
-				coauthors = CDSI.getCoauthors(theAuthor);
-			} catch (RemoteException e) {
-				// TODO Auto-generated catch block
-				JOptionPane.showMessageDialog(this,
-						"Um, this isn't really supposed to happen",
-						"Error!",JOptionPane.ERROR_MESSAGE);
-			}
-			
-			buildList();
-		}
-		
-		
-		private void buildList() {
-			buildListHelper();
-			coauthorList.addListSelectionListener(this);
-			coauthorList.setLayoutOrientation(JList.VERTICAL);
-			coauthorList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);			
-			JScrollPane listScroller = new JScrollPane(coauthorList);
-			listScroller.setPreferredSize(new Dimension(230, 80));
-			add(listScroller);
-		}
-		
-		private void buildListHelper() {
-			int i = 0;			
-			while (i < coauthors.size()){
-				listModel.add(i, coauthors.get(i));
-				i++;
-			}
-		}
-		
-		private void printTest() {
-			int capacity = coauthors.size();
-			int i = 0;
-			while (i < capacity) {
-				System.out.println(coauthors.get(i));
-				i++;
-			}
-		}
-		
-		@Override
-		public void valueChanged(ListSelectionEvent e) {
-			if (!e.getValueIsAdjusting()) {
-				String selection = (String) listModel.get(coauthorList.getSelectedIndex());
-				List<String> author = new ArrayList<String>();
-				author.add(selection);
-				Services.getBrowser().go(new AuthorSearchResults(author));
-			}			
-		}
-	}
-	
-	
 	
 	public AuthorSearchResults(List<String> queries) {
 		this.queries = queries;
 		System.out.println("Moved to search pane");
 		this.setVisible(true);
-		if(queries.size() == 1)
+		if(queries.size() == 1) {
 			add(new SingleAuthorSearchResult(queries.get(0)));
-		else
+			add(new SingleAuthorSearchArticleResult(queries.get(0)));
+		} else
 			add(new SingleAuthorSearchResult(queries.get(0)));
 	}
 	
@@ -126,5 +49,154 @@ public class AuthorSearchResults extends BrowserPage {
 		List<String> authors = new ArrayList<String>();
 		authors.add(str);
 		return authors;
-	}	
+	}
+	
+	
+	
+
+	/*
+	 * Creates list of articles the author has worked on (as THE author)
+	 */
+	private class SingleAuthorSearchArticleResult extends JPanel 
+												implements ListSelectionListener {
+		private String theAuthor;
+		private JList pubList;
+		private List<Publication> publications;
+		private DefaultListModel listModel;
+		
+		public SingleAuthorSearchArticleResult(String author) {
+			theAuthor = author;
+			initialize();
+		}
+		
+		private void initialize() {
+			setVisible(true);
+
+			listModel = new DefaultListModel();
+			pubList = new JList(listModel);
+			try {
+				publications = CDSI.getPublicationsForAnyAuthor(theAuthor);
+			} catch (RemoteException e) {
+				JOptionPane.showMessageDialog(this,
+						"Um, this isn't really supposed to happen",
+						"Error!",JOptionPane.ERROR_MESSAGE);
+			}
+			buildPubList();
+			
+		}
+		
+		private void buildPubList() {
+			int i = 0;			
+			while (i < publications.size()){
+				listModel.add(i, publications.get(i).getTitle());
+				i++;
+			}
+			pubList.addListSelectionListener(this);
+			pubList.setLayoutOrientation(JList.VERTICAL);
+			pubList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);			
+			JScrollPane listScroller = new JScrollPane(pubList);
+			listScroller.setPreferredSize(new Dimension(230, 80));
+			add(listScroller);			
+		}
+		
+		public void valueChanged(ListSelectionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	
+	
+	/*
+	 * This will produce the search result screen with single-entry
+	 * 	author search.
+	 */
+	private class SingleAuthorSearchResult extends JPanel implements ListSelectionListener{
+		private String theAuthor; //the query(author name)
+		private List<String> coauthors;
+		private DefaultListModel listModel;
+		private JList coauthorList;
+		
+		/*
+		 * Constructor
+		 */
+		public SingleAuthorSearchResult(String author) {
+			this.theAuthor = author;
+			initialize();
+		}
+		
+		private void initialize() {
+			setVisible(true);			
+			
+			//The single entry query
+			JLabel author = new JLabel(theAuthor);
+			add(author);
+			
+			//Set up the co-author list
+			listModel = new DefaultListModel();
+			coauthorList = new JList(listModel);
+			try {
+				coauthors = CDSI.getCoauthors(theAuthor);
+			} catch (RemoteException e) {
+				JOptionPane.showMessageDialog(this,
+						"Um, this isn't really supposed to happen",
+						"Error!",JOptionPane.ERROR_MESSAGE);
+			}			
+			buildCoauthorList();
+		}
+		
+		/*
+		 * Builds the co-author list
+		 */
+		private void buildCoauthorList() {
+			buildListHelper();
+			coauthorList.addListSelectionListener(this);
+			coauthorList.setLayoutOrientation(JList.VERTICAL);
+			coauthorList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);			
+			JScrollPane listScroller = new JScrollPane(coauthorList);
+			listScroller.setPreferredSize(new Dimension(230, 80));
+			add(listScroller);
+		}
+		/*
+		 * add author names to the list
+		 */
+		private void buildListHelper() {
+			int i = 0;			
+			while (i < coauthors.size()){
+				listModel.add(i, coauthors.get(i));
+				i++;
+			}
+		}
+		
+		/*
+		 * Testing purpose. Will be removed later
+		 */
+		private void printTest() {
+			int capacity = coauthors.size();
+			int i = 0;
+			while (i < capacity) {
+				System.out.println(coauthors.get(i));
+				i++;
+			}
+		}
+		
+		
+		public void valueChanged(ListSelectionEvent e) {
+			if (!e.getValueIsAdjusting()) {
+				//Need to make the selection as a List, since the method provided
+				//in CoauthorDataServiceInterface.java only takes List<String>
+				String selection = (String) listModel.get(coauthorList.getSelectedIndex());
+				List<String> author = new ArrayList<String>();
+				author.add(selection);
+				
+				//get the new browser
+				Services.getBrowser().go(new AuthorSearchResults(author));
+			}			
+		}
+	}
+	
+	
+	
+	
 }
