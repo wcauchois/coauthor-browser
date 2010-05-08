2e9b178ad0a9646e420a2c05654bfeecfe8ce7d1
Kevin Bang <yusbang@gmail.com>
2010-05-07 23:34:27 -0700

    FilterPanel works as a seperate class now

---
+++ src/edu/washington/cs/cse403d/coauthor/shared/CoauthorDataServiceInterface.java
@@ -0,0 +1,126 @@
+package edu.washington.cs.cse403d.coauthor.shared;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import edu.washington.cs.cse403d.coauthor.shared.model.PathLink;
+import edu.washington.cs.cse403d.coauthor.shared.model.Publication;
+
+/**
+ * Defines the interface of the remote data provider.
+ * 
+ * @author Jeff Prouty
+ * @version 2
+ */
+public interface CoauthorDataServiceInterface extends Remote {
+	public String SERVICE_NAME = "CoauthorDataServiceV2";
+
+	/**
+	 * Get a list of authors that match a fulltext search.
+	 * 
+	 * The given query is split on whitespace and then only authors that match
+	 * ALL terms (prefix atleast) are returned.
+	 * 
+	 * @param searchQuery
+	 * @param limit
+	 *            Number of suggestions to limit to.
+	 * @return
+	 * @throws RemoteException
+	 */
+	public List<String> getAuthors(String searchQuery) throws RemoteException;
+	
+	/**
+	 * Get a list of authors that match a fulltext search. Limits the search to only 50 to give low latency results.
+	 * 
+	 * The given query is split on whitespace and then only authors that match
+	 * ALL terms (prefix atleast) are returned.
+	 * 
+	 * @param searchQuery
+	 * @return
+	 * @throws RemoteException
+	 */
+	public List<String> getAuthorSuggestions(String searchQuery) throws RemoteException;
+
+	/**
+	 * Given a valid author, get a list of coauthors with a coauthor distance of
+	 * 1 from this author. This query can probably be optimized more.
+	 * 
+	 * @param author
+	 * @return
+	 * @throws RemoteException
+	 */
+	public List<String> getCoauthors(String author) throws RemoteException;
+
+	/**
+	 * Finds one shortest path (if it exists) between authorName1 and
+	 * authorName2. Returns a list of PathLink objects. Only will the PathLink
+	 * objects have publication lists populated if the populatePublications
+	 * argument is true.
+	 * 
+	 * @param authorName1
+	 * @param authorName2
+	 * @param populatePublications
+	 * @return Null if no path found.
+	 * @throws RemoteException
+	 */
+	public List<PathLink> getOneShortestPathBetweenAuthors(String authorName1, String authorName2,
+			boolean populatePublications) throws RemoteException;
+
+	/**
+	 * Finds all shortest paths (if it exists) between authorName1 and
+	 * authorName2. Returns a list of the shortest paths. Each shortest path is
+	 * a list of PathLink objects. Only will the PathLink objects have
+	 * publication lists populated if the populatePublications argument is true.
+	 * 
+	 * @param authorName1
+	 * @param authorName2
+	 * @param populatePublications
+	 * @return Null if no path(s) found.
+	 * @throws RemoteException
+	 */
+	public List<List<PathLink>> getAllShortestPathsBetweenAuthors(String authorName1, String authorName2,
+			boolean populatePublications) throws RemoteException;
+
+	/**
+	 * Given a list of authors, find the set of publications that all authors
+	 * have collaborated on.
+	 * 
+	 * @param author
+	 * @return
+	 * @throws RemoteException
+	 */
+	public List<Publication> getPublicationsForAllAuthors(String... author) throws RemoteException;
+
+	/**
+	 * Given a list of authors, find the set of publications that any of the
+	 * authors have collaborated on.
+	 * 
+	 * @param author
+	 * @return
+	 * @throws RemoteException
+	 */
+	public List<Publication> getPublicationsForAnyAuthor(String... author) throws RemoteException;
+
+	/**
+	 * Given a String of words, search for publications whose title contains all
+	 * of the given words (as prefixes). Very much like the getAuthors method.
+	 * 
+	 * @param searchQuery
+	 * @return
+	 * @throws RemoteException
+	 */
+	public List<String> getPublicationTitles(String searchQuery) throws RemoteException;
+	
+	/**
+	 * Given a String of words, search for publications whose title contains all
+	 * of the given words (as prefixes). Very much like the getAuthors method. Limits the search to only 50 to give low latency results.
+	 * 
+	 * @param searchQuery
+	 * @param limit
+	 *            Number of suggestions to limit to.
+	 * @return
+	 * @throws RemoteException
+	 */
+	public List<String> getPublicationTitleSuggestions(String searchQuery) throws RemoteException;
+}
---
+++ src/edu/washington/cs/cse403d/coauthor/uiprototype/ResourceManager.java
@@ -0,0 +1,84 @@
+package edu.washington.cs.cse403d.coauthor.uiprototype;
+
+import java.io.FileReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+public class ResourceManager {
+	private Map<String, Object> resources = new HashMap<String, Object>();
+	private String pathPrefix;
+	private URL getResourceURL(String name) {
+		return getClass().getResource(pathPrefix + name);
+	}
+	public ImageIcon loadImageIcon(String name) {
+		if(resources.containsKey(name))
+			return (ImageIcon)resources.get(name);
+		else {
+			URL url = getResourceURL(name);
+			if(url == null)
+				throw new ResourceException(name);
+			return new ImageIcon(url, name);
+		}
+	}
+	private static class ResourceException extends RuntimeException {
+		public ResourceException(String resourceName, Throwable cause) {
+			super("Couldn't load resource " + resourceName, cause);
+		}
+		public ResourceException(String resourceName) {
+			this(resourceName, null);
+		}
+	}
+	private static class StringsBuilder extends DefaultHandler {
+		private Map<String, String> strings;
+		private String currentString = null;
+		private String currentText = null;
+		public StringsBuilder(Map<String, String> strings) {
+			this.strings = strings;
+		}
+		@Override
+		public void startElement(String uri, String localName, String qName, Attributes attributes) {
+			if(localName == "string")
+				currentString = attributes.getValue("name");
+		}
+		@Override
+		public void characters(char[] ch, int start, int length) {
+			currentText = String.copyValueOf(ch, start, length).trim();
+		}
+		@Override
+		public void endElement(String uri, String localName, String qName) {
+			if(localName == "string")
+				strings.put(currentString, currentText);
+		}
+	}
+	@SuppressWarnings("unchecked")
+	public Map<String, String> loadStrings(String name) {
+		if(resources.containsKey(name))
+			return (Map<String, String>)resources.get(name);
+		else {
+			URL url = getResourceURL(name);
+			Map<String, String> strings = new HashMap<String, String>();
+			try {
+				XMLReader xr = XMLReaderFactory.createXMLReader();
+				StringsBuilder handler = new StringsBuilder(strings);
+				xr.setContentHandler(handler);
+				xr.setErrorHandler(handler);
+				xr.parse(new InputSource(new FileReader(url.getFile())));
+			} catch(Throwable t) {
+				throw new ResourceException(name, t);
+			}
+			return strings;
+		}
+	}
+	public ResourceManager(String pathPrefix) {
+		this.pathPrefix = pathPrefix;
+	}	
+}
