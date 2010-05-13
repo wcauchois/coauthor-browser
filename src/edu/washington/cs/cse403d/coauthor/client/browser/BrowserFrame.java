9ba842c22fbfc8fc7c65730f77c05aea0e43655b
William Cauchois <wcauchois@gmail.com>
2010-05-13 11:47:57 -0700

    organize imports in BrowserFrame

--- src/edu/washington/cs/cse403d/coauthor/client/browser/BrowserFrame.java
+++ src/edu/washington/cs/cse403d/coauthor/client/browser/BrowserFrame.java
@@ -7,11 +7,9 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import edu.washington.cs.cse403d.coauthor.client.Services;
