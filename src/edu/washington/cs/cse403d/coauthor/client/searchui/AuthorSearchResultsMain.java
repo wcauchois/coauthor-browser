8b8a510693e832a27bd6cf5fadd5d56e336a2980
William Cauchois <wcauchois@gmail.com>
2010-05-13 11:34:37 -0700

    improved the titles for several pages and added StringUtils

--- src/edu/washington/cs/cse403d/coauthor/client/searchui/AuthorSearchResultsMain.java
+++ src/edu/washington/cs/cse403d/coauthor/client/searchui/AuthorSearchResultsMain.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import edu.washington.cs.cse403d.coauthor.client.browser.BrowserPage;
+import edu.washington.cs.cse403d.coauthor.client.utils.StringUtils;
 
 public class AuthorSearchResultsMain extends BrowserPage {
 	private List<String> queries;
@@ -40,7 +41,12 @@ public class AuthorSearchResultsMain extends BrowserPage {
 	}
 	
 	public String getTitle() {
-		return "Results for \"" + queries.get(0) + "\"";
+		if(queries.size() == 1) {
+			return queries.get(0);
+		} else {
+			return StringUtils.elide(
+					StringUtils.join(", ", queries), 20);
+		}
 	}
 	@SuppressWarnings("unchecked")
 	public Class[] getCrumbs() {
