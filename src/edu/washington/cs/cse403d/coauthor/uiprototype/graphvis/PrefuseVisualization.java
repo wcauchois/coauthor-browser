c6ecef3d7441ab4dc7f47bc2756b35ea04986910
Miles Sackler <sackler.miles@gmail.com>
2010-05-19 10:09:50 -0700

    Made the graph feature work with the UI (had to fix after a merging issue).

--- src/edu/washington/cs/cse403d/coauthor/uiprototype/graphvis/PrefuseVisualization.java
+++ src/edu/washington/cs/cse403d/coauthor/uiprototype/graphvis/PrefuseVisualization.java
@@ -46,16 +46,65 @@ import prefuse.visual.VisualItem;
 public class PrefuseVisualization {
 	
 	public static final String HOSTNAME = "attu2.cs.washington.edu";
-
+	private JFrame frame;
+	private Display d;
 	
-
+	public PrefuseVisualization()
+	{
+		String s = "Jessica Miller";
+		String cur = "";
+		VisualCoAuthorExplorer vx = new VisualCoAuthorExplorer(s);
+	//	frame = new JFrame("prefuse example");
+		
+	//	frame.setBounds(800, 100, 1, 1);
+	//	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	//	frame.add(vx.getDisplay());
+	//	frame.pack();           // layout components in window
+	//	frame.setVisible(true); // show the window
+		vx.updateVis();
+		cur = s;
+		cur = s;
+		s = vx.curSelected;
+		d = vx.getDisplay();
+	}
 	
+	public PrefuseVisualization(String name)
+	{
+		String s = name;
+		String cur = "";
+		VisualCoAuthorExplorer vx = new VisualCoAuthorExplorer(s);
+	//	frame = new JFrame("prefuse example");
+		
+	//	frame.setBounds(800, 100, 1, 1);
+	//	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	//	frame.add(vx.getDisplay());
+	//	frame.pack();           // layout components in window
+	//	frame.setVisible(true); // show the window
+		vx.updateVis();
+		cur = s;
+		cur = s;
+		s = vx.curSelected;
+		d = vx.getDisplay();
+	}
+	
+	public JFrame getFrame()
+	{
+		return frame;
+	}
+	
+	public Display getD()
+	{
+		return d;
+	}
+	/*
 	public static void main(String[] args) {
 		String s = "Jessica Miller";
 		String cur = "";
 		VisualCoAuthorExplorer vx = new VisualCoAuthorExplorer(s);
 		// create a new window to hold the visualization
-		JFrame frame = new JFrame("prefuse example");
+		frame = new JFrame("prefuse example");
 		
 //		while(true)
 //		{
@@ -63,7 +112,7 @@ public class PrefuseVisualization {
 //			{
 			/*	frame.remove(vx.getDisplay());
 				vx = new VisualCoAuthorExplorer(s);
-			*/	// create a new window to hold the visualization
+				// create a new window to hold the visualization
 				//frame = new JFrame("prefuse example");
 				frame.setBounds(800, 100, 1, 1);
 			    // ensure application exits when window is closed
@@ -76,10 +125,11 @@ public class PrefuseVisualization {
 				cur = s;
 //			}
 			cur = s;
-//			s = vx.name;
+			s = vx.curSelected;
 //		}
 			
-    }
+    } 
+	*/
 	
 
 }
