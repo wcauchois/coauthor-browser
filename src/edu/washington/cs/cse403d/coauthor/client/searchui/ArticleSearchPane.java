fe9bc39423b748691a8849fc4d573d52a8e51fb0
salekhnovich <salekhnovich@localhost>
2010-06-01 17:55:56 -0700

    Merge with 98fc7c2c2dfaafde809b63a76c1c04db167f59ae

---
+++ src/edu/washington/cs/cse403d/coauthor/client/searchui/ArticleSearchPane.java
@@ -0,0 +1,48 @@
+package edu.washington.cs.cse403d.coauthor.client.searchui;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import edu.washington.cs.cse403d.coauthor.client.Services;
+import edu.washington.cs.cse403d.coauthor.client.utils.Fonts;
+import edu.washington.cs.cse403d.coauthor.client.utils.HelpMarker;
+
+public class ArticleSearchPane extends JPanel {
+	private static final long serialVersionUID = -2380014657227857630L;
+	
+	protected JTextField query = new JTextField();
+	protected JButton submit = new JButton("Search");
+	public ArticleSearchPane() {
+		JPanel topPart = new JPanel();
+		topPart.add(new JLabel("Please enter your search terms below"));
+		topPart.add(new HelpMarker("Click here for more information."));
+		
+		JPanel bottomPart = new JPanel();
+		query.setPreferredSize(new Dimension(100, query.getPreferredSize().height));
+		query.setFont(Fonts.getSearchFont());
+		query.setPreferredSize(new Dimension(150, 24));
+		bottomPart.add(query);
+		bottomPart.add(submit);
+		submit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				Services.getBrowser().go(new ArticleSearchResults(query.getText()));
+			}
+		});
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		add(topPart);
+		add(bottomPart);
+	}
+	
+	public String getTitle()
+	{
+		return "Search";
+	}
+}
