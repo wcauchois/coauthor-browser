db7d7262d6c1d7c231806792fa7566b5214667b5
jeff.prouty <jeff.prouty@localhost>
2010-06-05 17:59:24 -0700

    Major refactor of AuthorResultPane. This make the look more consistent between the different types of author search.
    Also fixed the bug where hovering over hyperlinks grow the size of the link by 1px.
    
    Also fixed some java warnings.
    
    Also fixed some indentation w/ eclipse formatter.

--- src/edu/washington/cs/cse403d/coauthor/client/searchui/AuthorResult.java
+++ src/edu/washington/cs/cse403d/coauthor/client/searchui/AuthorResult.java
@@ -1,15 +1,29 @@
 package edu.washington.cs.cse403d.coauthor.client.searchui;
 
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+
+import edu.washington.cs.cse403d.coauthor.client.Services;
 import edu.washington.cs.cse403d.coauthor.client.browser.BrowserPage;
 import edu.washington.cs.cse403d.coauthor.client.browser.MessagePage;
 import edu.washington.cs.cse403d.coauthor.client.browser.PageLoadError;
 import edu.washington.cs.cse403d.coauthor.client.utils.GoBackActionListener;
+import edu.washington.cs.cse403d.coauthor.client.utils.HyperLinkButton;
 import edu.washington.cs.cse403d.coauthor.client.utils.StringUtils;
+import edu.washington.cs.cse403d.coauthor.shared.model.Publication;
 
 /**
  * This page displays search results information for single or multiple authors.
@@ -19,7 +33,7 @@ import edu.washington.cs.cse403d.coauthor.client.utils.StringUtils;
 public class AuthorResult extends BrowserPage {
 	private static final long serialVersionUID = -3718649382487574216L;
 
-	private List<String> queries;
+	private List<String> authorNames;
 
 	/**
 	 * Constructor for single author search case
@@ -37,49 +51,149 @@ public class AuthorResult extends BrowserPage {
 	 * @param queries
 	 */
 	public AuthorResult(List<String> queries) {
-		this.queries = queries;
+		this.authorNames = queries;
 	}
 
 	@Override
 	protected void load() throws PageLoadError {
-		this.setVisible(true);
+		setVisible(true);
 		setLayout(new GridBagLayout());
+
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
 		c.gridy = 0;
 
 		try {
-			if (queries.size() == 1) {
-				add(new AuthorCoauthorResult(queries.get(0)), c);
-			} else
-				add(new AuthorCoauthorResult(queries), c);
-	
-			c.gridx = 0;
-			c.gridy = 1;
-			if (queries.size() == 1)
-				add(new AuthorPublicationResult(queries.get(0)), c);
-			else
-				add(new AuthorPublicationResult(queries), c);
-		} catch(Exception e) {
-			MessagePage message = new MessagePage(
-					MessagePage.ERROR,
-					"Failed to complete search",
-					"There was an error while attempting to complete your search (" +
-					e.getMessage() + ").",
+			switch (authorNames.size()) {
+			case 0:
+				throw new IllegalStateException("No authors to search over");
+			case 1:
+				add(createAuthorHeader(authorNames.get(0), false), c);
+				c.gridy++;
+				// Single Author Results
+				// Add the CoauthorResults
+				add(new AuthorCoauthorResult(authorNames.get(0)), c);
+				c.gridy++;
+				// Add the PublicationResults
+				add(new AuthorPublicationResult(authorNames), c);
+				break;
+			case 2:
+				// Multi Author Publication Search if exists
+				// ELSE Chain Search
+				// Add the Multi Author Header
+				add(createMultiAuthorHeader(authorNames), c);
+				c.gridy++;
+				try {
+					List<Publication> groupPublications = Services.getCoauthorDataServiceInterface()
+							.getPublicationsForAllAuthors(authorNames.toArray(new String[0]));
+
+					if (groupPublications.isEmpty()) {
+						// Add the chain search panel
+						add(new ChainSearchResult(authorNames.get(0), authorNames.get(1)), c);
+					} else {
+						// Results in two queries being performed. However, they
+						// are IDENTICAL and therefore will have cache hit with
+						// low latency.
+						// Add the publications shared by these authors
+						add(new AuthorPublicationResult(authorNames), c);
+					}
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+				break;
+			default:
+				// Multi Author Publication Search
+				add(createMultiAuthorHeader(authorNames), c);
+				c.gridy++;
+				// Add the publications shared by these authors
+				add(new AuthorPublicationResult(authorNames), c);
+				break;
+			}
+		} catch (Exception e) {
+			MessagePage message = new MessagePage(MessagePage.ERROR, "Failed to complete search",
+					"There was an error while attempting to complete your search (" + e.getMessage() + ").",
 					MessagePage.GO_BACK);
 			message.addActionListener(new GoBackActionListener());
 			throw new PageLoadError(message);
 		}
-		
+
 		setLoaded();
 	}
 
-	public String getTitle() {
-		if (queries.size() == 1) {
-			return queries.get(0);
+	private JPanel createAuthorHeader(final String author, boolean asLink) {
+		JPanel result = new JPanel();
+		result.setLayout(new BoxLayout(result, BoxLayout.Y_AXIS));
+		result.setVisible(true);
+
+		// The Author Name.
+		JLabel title = new JLabel("Author");
+		Font f = title.getFont();
+		float s = title.getFont().getSize2D();
+		s += 8.0f;
+		title.setFont(f.deriveFont(s));
+		result.add(title);
+
+		result.add(new JSeparator(SwingConstants.HORIZONTAL));
+
+		if (asLink) {
+			HyperLinkButton authorLink = new HyperLinkButton(author);
+			authorLink.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					Services.getBrowser().go(new AuthorResult(author));
+				}
+			});
+			result.add(authorLink);
 		} else {
-			return StringUtils.elide(StringUtils.join(", ", queries), 20);
+			JLabel authorLabel = new JLabel(author);
+			result.add(authorLabel);
+		}
+		
+		result.add(Box.createVerticalStrut(10));
+
+		return result;
+	}
+
+	private JPanel createMultiAuthorHeader(List<String> authorNames) {
+		JPanel result = createAuthorHeader(authorNames.get(0), true);
+
+		// Coauthors
+		JLabel coauthor = new JLabel(authorNames.size() > 2 ? "Coauthors" : "Coauthor");
+		Font f = coauthor.getFont();
+		float s = coauthor.getFont().getSize2D();
+		s += 8.0f;
+		coauthor.setFont(f.deriveFont(s));
+		result.add(coauthor);
+		result.add(new JSeparator(SwingConstants.HORIZONTAL));
+
+		for (int i = 1; i < authorNames.size(); ++i) {
+			result.add(createCoauthorLink(authorNames.get(i)));
+			result.add(Box.createVerticalStrut(3));
+		}
+		result.add(Box.createVerticalStrut(10));
+
+		return result;
+	}
+
+	/**
+	 * Create a formatted display of coauthors for multi author search result
+	 */
+	private HyperLinkButton createCoauthorLink(String coauthorName) {
+		final HyperLinkButton coauthor = new HyperLinkButton(coauthorName);
+		coauthor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				Services.getBrowser().go(new AuthorResult(Arrays.asList(coauthor.getText())));
+			}
+		});
+		return coauthor;
+	}
+
+	public String getTitle() {
+		if (authorNames.size() == 1) {
+			return authorNames.get(0);
+		} else {
+			return StringUtils.elide(StringUtils.join(", ", authorNames), 20);
 		}
 	}
 }
--- src/edu/washington/cs/cse403d/coauthor/client/searchui/ChainSearchResult.java
+++ src/edu/washington/cs/cse403d/coauthor/client/searchui/ChainSearchResult.java
@@ -4,12 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.Box;
@@ -22,111 +19,91 @@ import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 
 import edu.washington.cs.cse403d.coauthor.client.Services;
-import edu.washington.cs.cse403d.coauthor.client.browser.BrowserPage;
 import edu.washington.cs.cse403d.coauthor.client.utils.FilterPanel;
-import edu.washington.cs.cse403d.coauthor.client.utils.HyperLinkButton;
+import edu.washington.cs.cse403d.coauthor.shared.CoauthorDataServiceInterface;
 import edu.washington.cs.cse403d.coauthor.shared.model.PathLink;
 
-public class ChainSearchResult extends BrowserPage {
+public class ChainSearchResult extends JPanel {
 	private static final long serialVersionUID = 7570009274799150945L;
-	
-	private edu.washington.cs.cse403d.coauthor.shared.CoauthorDataServiceInterface CDSI = Services
-		.getCoauthorDataServiceInterface();
-	
+
+	private CoauthorDataServiceInterface CDSI = Services.getCoauthorDataServiceInterface();
+
 	private List<PathLink> chain;
 	private DefaultListModel listModel;
 	private JList chainList;
-	
-	private JPanel contentPanel;
-	
-	private String author1;
-	private String author2;
-	
+
+	private final String author1;
+	private final String author2;
+
 	/**
 	 * Constructor for single author search result screen
-	 *
+	 * 
 	 * @param author
 	 *            the author that was entered as the query
 	 */
-	public ChainSearchResult(final String author1, final String author2) {
+	public ChainSearchResult(String author1, String author2) {
+		setLayout(new BorderLayout());
+		setVisible(true);
+
 		this.author1 = author1;
 		this.author2 = author2;
-	}
 
-	@Override
-	protected void load() {
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		setVisible(true);
-		setLayout(new BorderLayout());
-		boolean valid = true;
-		try {
-			chain = CDSI.getOneShortestPathBetweenAuthors(author1, author2, getAutoscrolls());
-		} catch (RemoteException e) {
-			System.out.println("Invalid author(s)");
-			valid = false;
-		}
-		if (valid)
-			singleEntryInitialize();
-		
-		setLoaded();
+		createChainPanel();
 	}
 
 	/**
 	 * Constructor for multi-entry author search result screen
-	 *
+	 * 
 	 * @param authorList
 	 *            list of queries(author names)
 	 */
 	/**
 	 * Internal helper method for single-author search result
 	 */
-	private void singleEntryInitialize() {
-		contentPanel = new JPanel();
-		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
-		contentPanel.setVisible(true);
-		
-		// Title
-		JLabel title = new JLabel("Results for Chain Search: ");
-		Font f = title.getFont();
-		float s = title.getFont().getSize2D();
-		s += 10.0f;
-		title.setFont(f.deriveFont(s));
-		contentPanel.add(title);
-		contentPanel.add(Box.createRigidArea(new Dimension(0, 5)));
-		
-		// Coauthor list label
-		JLabel authorChainLabel = new JLabel("Author Chain:");
-		f = authorChainLabel.getFont();
-		s = authorChainLabel.getFont().getSize2D();
-		s += 6.0f;
-		authorChainLabel.setFont(f.deriveFont(s));
-		contentPanel.add(authorChainLabel);
-		
-		// Coauthor list
-		listModel = new DefaultListModel();
-		chainList = new JList(listModel);
-		chainList.setFont(chainList.getFont().deriveFont(Font.PLAIN));
-		
-		buildChain();
-		
-		JPanel number = new JPanel();
-		JLabel numberText = new JLabel("The number of authors in this chain is: ");
-		JLabel numberVal = new JLabel("" + listModel.size());
-		numberText.setFont(f.deriveFont(s));
-		numberVal.setFont(f.deriveFont(s));
-		number.add(numberText);
-		number.add(numberVal);
-		
-		FilterPanel filterPanel = new FilterPanel(chainList, author1, author2);
-		buildNavigator(filterPanel);
-		add(filterPanel);
-		add(contentPanel, BorderLayout.PAGE_START);
-		add(number, BorderLayout.PAGE_END);
+	private void createChainPanel() {
+		try {
+			chain = CDSI.getOneShortestPathBetweenAuthors(author1, author2, getAutoscrolls());
+
+			JPanel contentPanel = new JPanel();
+			contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+			contentPanel.setVisible(true);
+
+			// Title
+			JLabel explanation = new JLabel("These authors have not collaborated yet");
+			contentPanel.add(explanation);
+			contentPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+			// Coauthor list label
+			JLabel authorChainLabel = new JLabel("Author Chain:");
+			Font f = authorChainLabel.getFont();
+			float s = authorChainLabel.getFont().getSize2D();
+			s += 8.0f;
+			authorChainLabel.setFont(f.deriveFont(s));
+			contentPanel.add(authorChainLabel);
+
+			// Coauthor list
+			listModel = new DefaultListModel();
+			chainList = new JList(listModel);
+			chainList.setFont(chainList.getFont().deriveFont(Font.PLAIN));
+
+			contentPanel.add(buildChain());
+
+			JPanel number = new JPanel();
+			JLabel numberText = new JLabel("The number of authors in this chain is: ");
+			JLabel numberVal = new JLabel("" + listModel.size());
+			numberText.setFont(f.deriveFont(s));
+			numberVal.setFont(f.deriveFont(s));
+			number.add(numberText);
+			number.add(numberVal);
+
+			FilterPanel filterPanel = new FilterPanel(chainList, author1, author2);
+			buildNavigator(filterPanel);
+			add(filterPanel);
+			add(contentPanel, BorderLayout.PAGE_START);
+			add(number, BorderLayout.PAGE_END);
+		} catch (RemoteException e) {
+			System.out.println("Invalid author(s)");
+		}
 	}
 
 	private void buildNavigator(FilterPanel filterPanel) {
@@ -135,55 +112,54 @@ public class ChainSearchResult extends BrowserPage {
 			@Override
 			public void mouseClicked(MouseEvent evt) {
 				int selected = theList.getSelectedIndex();
-				
+
 				String searchFor = ("<html><i>°ÊSearch for this author</i></html>");
 				String closeMenu = ("<html><i>°ÊClose this submenu</i></html>");
-					if(!theList.isSelectionEmpty() && !theList.getSelectedValue().equals(closeMenu) &&
-							!theList.getSelectedValue().equals(searchFor)){
-						if( selected + 1 == listModel.getSize() ||
-								listModel.getElementAt(selected + 1) != searchFor) {
-							
-							selected = theList.getSelectedIndex();
-							listModel.insertElementAt(searchFor, selected + 1);
-							listModel.insertElementAt(closeMenu, selected + 2);
-							
-							theList.setModel(listModel);
-							theList.setSelectedIndex(selected);
-						}
-					}
-					
-					if(!theList.isSelectionEmpty() && theList.getSelectedValue().equals(closeMenu)){
-						listModel.remove(selected);
-						theList.setSelectedIndex(selected -1);
-						listModel.remove(theList.getSelectedIndex());
+				if (!theList.isSelectionEmpty() && !theList.getSelectedValue().equals(closeMenu)
+						&& !theList.getSelectedValue().equals(searchFor)) {
+					if (selected + 1 == listModel.getSize() || listModel.getElementAt(selected + 1) != searchFor) {
+
+						selected = theList.getSelectedIndex();
+						listModel.insertElementAt(searchFor, selected + 1);
+						listModel.insertElementAt(closeMenu, selected + 2);
+
 						theList.setModel(listModel);
+						theList.setSelectedIndex(selected);
 					}
-					
-					int subMenuSelection;
-					String selectedItem;
-					if(!theList.isSelectionEmpty()) {
-						subMenuSelection = theList.getSelectedIndex();
-						selectedItem = (String) listModel.getElementAt(subMenuSelection);
-					} else {
-						subMenuSelection = selected - 2;
-						selectedItem = "";
-					}                              
-					
-					if (selectedItem.equals(searchFor)) {
-						String author = (String) listModel.getElementAt(subMenuSelection - 1);
-						
-						//Remove the submenu before navigating
-						listModel.remove(subMenuSelection);
-						theList.setSelectedIndex(subMenuSelection);
-						listModel.remove(theList.getSelectedIndex());
-						theList.setModel(listModel);
-						
-						Services.getBrowser().go(new AuthorResult(author));                            
-					}
-					if(evt.getClickCount() == 2) {
-						String author = (String)theList.getSelectedValue();
-						Services.getBrowser().go(new AuthorResult(author));
-					}
+				}
+
+				if (!theList.isSelectionEmpty() && theList.getSelectedValue().equals(closeMenu)) {
+					listModel.remove(selected);
+					theList.setSelectedIndex(selected - 1);
+					listModel.remove(theList.getSelectedIndex());
+					theList.setModel(listModel);
+				}
+
+				int subMenuSelection;
+				String selectedItem;
+				if (!theList.isSelectionEmpty()) {
+					subMenuSelection = theList.getSelectedIndex();
+					selectedItem = (String) listModel.getElementAt(subMenuSelection);
+				} else {
+					subMenuSelection = selected - 2;
+					selectedItem = "";
+				}
+
+				if (selectedItem.equals(searchFor)) {
+					String author = (String) listModel.getElementAt(subMenuSelection - 1);
+
+					// Remove the submenu before navigating
+					listModel.remove(subMenuSelection);
+					theList.setSelectedIndex(subMenuSelection);
+					listModel.remove(theList.getSelectedIndex());
+					theList.setModel(listModel);
+
+					Services.getBrowser().go(new AuthorResult(author));
+				}
+				if (evt.getClickCount() == 2) {
+					String author = (String) theList.getSelectedValue();
+					Services.getBrowser().go(new AuthorResult(author));
+				}
 			}
 		});
 	}
@@ -191,17 +167,16 @@ public class ChainSearchResult extends BrowserPage {
 	/**
 	 * Builds the co-author list for single author search result
 	 */
-	private void buildChain() {
+	private JScrollPane buildChain() {
 		buildListHelper();
 		chainList.setLayoutOrientation(JList.VERTICAL);
 		chainList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		chainList.setVisibleRowCount(5);
 		JScrollPane listScroller = new JScrollPane(chainList);
 		listScroller.setAlignmentX(Component.LEFT_ALIGNMENT);
-		contentPanel.add(listScroller);
+		return listScroller;
 	}
-	
-	
+
 	/**
 	 * add author names to the JList, single author search case
 	 */
@@ -212,8 +187,9 @@ public class ChainSearchResult extends BrowserPage {
 			i++;
 		}
 		listModel.add(i, chain.get(i - 1).toString().substring(chain.get(i - 1).toString().indexOf('>') + 1));
-		author2 = chain.get(i - 1).toString().substring(chain.get(i - 1).toString().indexOf('>') + 1);
-		this.author1 = (String) listModel.get(0);
+		// author2 = chain.get(i - 1).toString().substring(chain.get(i -
+		// 1).toString().indexOf('>') + 1);
+		// this.author1 = (String) listModel.get(0);
 	}
 
 	public String getTitle() {
